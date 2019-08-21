aa93eefd060da3dcf24592d7b2bc7481363d1938
APCS2nd <APCS2nd@MHS-LT9-27>
2019-08-21 12:17:16 -0700

    complete hello world project

---
+++ Hello World Project/src/HelloWorld2.java
@@ -0,0 +1,13 @@
+//Bryce Wang
+//August 16, 2019
+//first class (iconic) to Java for APCS
+//prints message to screen
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+
+}
