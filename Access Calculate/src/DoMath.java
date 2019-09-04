4062e56eeacb693866d981328c4af6dee33036bd
APCS2nd <APCS2nd@MHS-LT9-27>
2019-09-04 12:28:37 -0700

    commit square and cube

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,14 @@
+/*Bryce Wang
+ * August 29, 2019
+ * Period 4
+ * Call math methods from the calculate library
+ * Client code or runner code
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(12));
+		System.out.println(Calculate.cube(2));
+	}
+
+}
--- Hello World Project/src/HelloWorld2.java
+++ Hello World Project/src/HelloWorld2.java
@@ -7,7 +7,28 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
+		printPikachu();
+		drawBox();
+	}
+	
+	public static void drawBox() {
+		printPlusMinusRow();
+		print2VertLinesRow();
+		print2VertLinesRow();
+		print2PlusMinusRow():
+		System.out.println();
+	}
+	
+	public static void printPlusMinusRow() {
+		System.out.println("+-------+");
+	}
+	
+	public static void print2VertLinesRow() {
+		System.out.println("|       |");
+	}
+	
+	public static void printPikachu() {
+		System.out.println("Pikachu code goes here");
 	}
 
 }
