dfe9d268d52decf88a1dce3b62e9a38573b641d3
APCS2nd <APCS2nd@MHS-LT9-27>
2019-09-26 11:55:55 -0700

    quadratic client

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -6,15 +6,63 @@
 
 public class Quadratic {
 	public static String quadrDescriber(double a, double b, double c) {
-		String graphDescip = "Description of the graph of: \n" + a + " x^2 + " + b + " x + " + c;
-		String direction;
+		String graphDescrip = "Description of the graph of: \ny = " + a + " x^2 + " + b + " x + " + c + "\n\n";
+		String direction = "Opens: ";
 		if(a > 0) {
-			direction = "Opens: Up";
-		} else if(a < 0) {
-			direction = "Opens: Down";
+			direction += "Up\n";
+		} else if (a < 0){
+			direction += "Down\n";
 		} else {
-			direction = "Not a quadratic";
+			direction = "Not a quadratic function";
 		}
-		String axisOfSym = "Axis of Symmetry: " + -b/(2*a);	
+		double x = (-b)/(2*a);
+		double y = a*x + b*x + c;
+		String axisOfSym = "Axis of Symmetry: " + x + "\n";
+		String vertex = "Vertex: (" + x + ", " + y + ")\n";
+		String xIntercept = "x-intercept(s): " + quadForm(a,b,c) + "\n";
+		String yIntercept = "y-intercept(s): " + c + "\n\n";
+		String keepGoing = "Do you want to keep going? (Type \"quit\" to end)";
+		return graphDescrip + direction + axisOfSym + vertex + xIntercept + yIntercept + keepGoing;
+	}
+
+	public static double discriminant(double a, double b, double c) {
+		double answer = 0;
+		answer = (b * b) - (4 * a * c);
+		return answer;
+	}
+	
+	public static double round2(double number) {
+		int rounded;
+		if(number > 0) {
+			rounded = (int) (number * 100 + 0.5);
+				return ((double) rounded) / 100;			
+		} else {
+			rounded = (int) (number * 100 - 0.5);
+			return ((double) rounded) / 100;
+		}
+	}
+	
+	public static double sqrt(double numToRoot) {
+		if(numToRoot < 0) {
+			throw new IllegalArgumentException("Cannot square root a negative number");
+		}
+		double sqrt = 0;
+		for(double i = 0; i * i <= numToRoot; i+=0.001) {
+			sqrt = i;
+		}
+		return round2(sqrt);
+	}
+	
+	public static String quadForm(double a, double b, double c) {
+		double discrim = discriminant(a,b,c);
+		if(discrim < 0) {
+			return "no real roots";
+		}
+		if(discrim == 0) {
+			return -b/(2*a) + "";
+		}
+		double firstRoot = (-b + sqrt(discrim))/(2*a);
+		double secondRoot = (-b - sqrt(discrim))/(2*a);
+		return round2(firstRoot) + " and " + round2(secondRoot);
 	}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,10 +3,29 @@
  * Period 4
  * Call quadratic methods and handles interactions with user.
  */
+import java.util.*;
 
 public class QuadraticClient {
 	public static void main(String[] args) {
-		
+		double a;
+		double b;
+		double c;
+		boolean quit = false;
+		System.out.println("Welcome to the Quadratic Describer");
+		System.out.println("Provide values for coefficients a, b, c\n");
+		Scanner userInput = new Scanner(System.in);
+		while(quit != true) {	
+			System.out.print("a: ");
+			a = userInput.nextDouble();
+			System.out.print("b: ");
+			b = userInput.nextDouble();
+			System.out.print("c: ");
+			c = userInput.nextDouble();
+			System.out.println(Quadratic.quadrDescriber(a, b, c));
+			if(userInput.next() == String.equal("quit")) {
+				quit = true;
+			}
+		}
+		userInput.close();
 	}
-
 }
