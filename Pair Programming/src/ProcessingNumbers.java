0d7bdd48071b6810fce8e77e695f577f7a0628dc
APCS2nd <APCS2nd@MHS-LT9-27>
2019-10-08 11:45:06 -0700

    pair programming

---
+++ Pair Programming/src/ProcessingNumbers.java
@@ -0,0 +1,40 @@
+/* Bryce Wang
+ * October 2, 2019
+ * Period 4
+ * Prints the smallest and largest of all the numbers supplied by user
+ * Print the sum of all even numbers along with the even number typed
+ */
+import java.util.*;
+
+public class ProcessingNumbers {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("How many numbers? ");
+		int first = userInput.nextInt();
+		int max = first;
+		int min = first;
+		int sum = 0;
+		int largestEven = 0;
+		for(int i = 1; i <= numNum; i++) {
+			System.out.print("Number " + i + ": ");
+			int num = userInput.nextInt();
+			if(num > max) {
+				max = num;
+			}
+			if(num < min) {
+				min = num;
+			}
+			if(num % 2 == 0) {
+				sum += num;
+				if(num > largestEven) {
+					largestEven = num;
+				}
+			}
+		}
+		System.out.println("The smallest number is: " + min);
+		System.out.println("The largest number is: " + max);
+		System.out.println("The sum of all even numbers is: " + sum);
+		System.out.println("The largest even number is: " + largestEven);
+		userInput.close();
+	}
+}
