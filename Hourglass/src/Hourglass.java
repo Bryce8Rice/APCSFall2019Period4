b4aa430cc3c427e3fd4df62af725040cec0ed329
APCS2nd <APCS2nd@MHS-LT9-27>
2019-10-28 11:56:59 -0700

    array

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,12 +1,24 @@
+/* Bryce Wang
+ * October 25, 2019
+ * Period 4
+ * Prints an hourglass
+ */
 import java.util.Scanner;
 
 public class Hourglass {
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-		System.out.print("How big is your hourglass? ");
-		int size = userInput.nextInt();
-		top(size);
-		
+		boolean quit = false;
+		while(!quit) {
+			System.out.print("How big is your hourglass? ");
+			int size = userInput.nextInt();
+			top(size);
+			bottom(size);
+			System.out.print("Do you want to quit? ");
+			if(userInput.next().equals("yes")) {
+				quit = true;
+			}
+		}
 		userInput.close();
 	}
 	
@@ -17,7 +29,7 @@ public class Hourglass {
 			top += "\"";
 		}
 		top += "|\n";
-		for(int line = 1; line <= size/2-1; line++) {
+		for(int line = 1; line <= size/2 - 1; line++) {
 			for(int space = 1; space <= line; space++) {
 				top += " ";
 			}
@@ -27,19 +39,34 @@ public class Hourglass {
 			}
 			top += "/\n";
 		}
-		for(int mid = 1; mid <= size/2; mid++) {
+		for(int space = 1; space <= size/2; space++) {
 			top += " ";
 		}
-		top += "||\n";
+		if(size % 2 == 0) {
+			top += "||\n";
+		} else {
+			top += "|||\n";
+		}
 		System.out.print(top);
 	}
 	
 	public static void bottom(int size) {
 		String bottom = "";
-		for(int line = 1; line <= size/2-1; line++) {
-			for(int space = line; space <= 1; line--) {
-				System.out.print(" ");
+		for(int line = size/2 - 1; line >= 1; line--) {
+			for(int space = line; space >= 1; space--) {
+				bottom += " ";
 			}
+			bottom += "/";
+			for(int colon = 1; colon <= -2*line+size; colon++) {
+				bottom += ":";
+			}
+			bottom += "\\\n";
 		}
+		bottom += "|";
+		for(int quote = 1; quote <= size; quote++) {
+			bottom += "\"";
+		}
+		bottom += "|\n";
+		System.out.print(bottom);
 	}
 }
---
+++ Pair Programming/src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	for(int line = 1; line <= size/2-1; line++) {
+				for(int space = line; space <= 1; line--) {
+					System.out.print(" ");
+				}
+			}
+}
+
