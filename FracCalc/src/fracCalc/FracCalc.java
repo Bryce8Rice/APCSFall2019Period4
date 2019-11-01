76637014f985a555b411384035cad3209422de9a
APCS2nd <APCS2nd@MHS-LT9-27>
2019-11-01 11:26:40 -0700

    frac calc

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -2,10 +2,10 @@ package fracCalc;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+  public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        
+    	// TODO: Read the input from the user and call produceAnswer with an equation
     }
     //gguygyugyug
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
