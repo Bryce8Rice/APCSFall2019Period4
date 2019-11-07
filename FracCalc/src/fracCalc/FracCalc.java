76787627efed55932369ef434050d1e5e597d5b5
APCS2nd <APCS2nd@MHS-LT9-27>
2019-11-07 11:56:44 -0800

    checkpoint 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -15,49 +15,34 @@ public class FracCalc {
         	}
         }
     }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+  
     public static String produceAnswer(String input){ 
-        // TODO: Implement this function to produce the solution to the input
         String operation[] = input.split(" ");
         String op1 = operation[0];
         String op2 = operation[2];
         String operator = operation[1];
-        String wholeOp1[] = {op1};
+        String wholeOp1[] = {"0", op1};
         if(op1.contains("_")) {
-        	wholeOp1[] = op1.split("_");
-        } else {
-        	wholeOp1[] = ;
+        	wholeOp1 = op1.split("_");
         }
-        if(wholeOp1.length == 2) {
-        	String fracOp1[] = wholeOp1[1].split("/");
-        } else {
-        	String fracOp1[] = wholeOp1[0];
+        String fracOp1[] = {"0", "1"};
+        if(wholeOp1[1].contains("/")) {
+        	fracOp1 = wholeOp1[1].split("/");
         }
+        String wholeOp2[] = {"0", op2};
         if(op2.contains("_")) {
-            String wholeOp2[] = op2.split("_");   	
-        } else {
-        	String wholeOp2[] = {op2};
+            wholeOp2 = op2.split("_");   	
         }
-        if(wholeOp2.length == 2) {
-            String fracOp2[] = wholeOp2[1].split("/");
-        } else {
-        	String fracOp2[] = {wholeOp2};
+        String fracOp2[] = {"0", "1"};
+        if(wholeOp2[1].contains("/")) {
+            fracOp2 = wholeOp2[1].split("/");
         }
+        String whole1 = wholeOp1[0];
         String numer1 = fracOp1[0];
         String denom1 = fracOp1[1];
+        String whole2 = wholeOp2[0];
         String numer2 = fracOp2[0];
         String denom2 = fracOp2[1];
-        return op2;
+        return "whole:" + whole2 + " numerator:" + numer2 + " denominator:" + denom2;
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
 }
