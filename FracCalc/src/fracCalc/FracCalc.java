d3e1f43f80d2e6e51149ea148aee73faf3dc8d77
Bryce8Rice <Bryce8Rice@DESKTOP-DH9NFP2>
2019-11-07 23:20:43 -0800

    finish checkpoint 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -21,21 +21,29 @@ public class FracCalc {
         String op1 = operation[0];
         String op2 = operation[2];
         String operator = operation[1];
-        String wholeOp1[] = {"0", op1};
+        String wholeOp1[] = {op1, "0"};
+        String fracOp1[] = {"0", "1"};
         if(op1.contains("_")) {
         	wholeOp1 = op1.split("_");
+        	if(wholeOp1[1].contains("/")) {
+        		fracOp1 = wholeOp1[1].split("/");
+        	}
+        } else if(wholeOp1[1].contains("/")) {
+        	fracOp1 = wholeOp1[0].split("/");
+        	wholeOp1[0] = "0";
+        	wholeOp1[1] = op1;
         }
-        String fracOp1[] = {"0", "1"};
-        if(wholeOp1[1].contains("/")) {
-        	fracOp1 = wholeOp1[1].split("/");
-        }
-        String wholeOp2[] = {"0", op2};
-        if(op2.contains("_")) {
-            wholeOp2 = op2.split("_");   	
-        }
+        String wholeOp2[] = {op2, "0"};
         String fracOp2[] = {"0", "1"};
-        if(wholeOp2[1].contains("/")) {
-            fracOp2 = wholeOp2[1].split("/");
+        if(op2.contains("_")) {
+        	wholeOp2 = op2.split("_");
+        	if(wholeOp2[1].contains("/")) {
+        		fracOp2 = wholeOp2[1].split("/");
+        	}
+        } else if(wholeOp2[0].contains("/")) {
+        	fracOp2 = wholeOp2[0].split("/");
+        	wholeOp2[0] = "0";
+        	wholeOp2[1] = op2;
         }
         String whole1 = wholeOp1[0];
         String numer1 = fracOp1[0];
